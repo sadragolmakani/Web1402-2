5516c1a787e98d02c12c5e70a4bc98426ce8a5b0
sadra <you@example.com>
2024-02-06 17:12:17 +0330

    new file season 01

---
+++ season 01/season.js
@@ -0,0 +1,8 @@
+console.log("Hellow wold");
+let a = 1;
+let B = 2;
+console.log ("Sum a + b:" , a+B);
+let x = 1;
+console.log("type x :" , typeof x);
+x = [1,2,"there","four",5];
+console.log(x[2]);
