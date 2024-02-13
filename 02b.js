eca954d0796d930eee032f0e2b6d98da04219bea
sadragolmakani <159141009+sadragolmakani@users.noreply.github.com>
2024-02-13 17:23:29 +0330

    02b

---
+++ 02b.js
@@ -0,0 +1,21 @@
+// let inputs = [
+// {
+//     name: naser,
+//     age : 18 
+// },
+// {
+//     name: reza,
+//     age : 32
+// },
+// {
+//     name: behroza,
+//     age : 11 
+// },
+// ]
+
+// for (let index = 0; index <= inputs.length; index++) {
+//     console.log(inputs[0])
+// 
+let name = prompt("Enter your name");
+let age = prompt("Enter your age");
+
